f177da507e27bc506321dadc8f82e6107037c078
Alan Hu <17.alanhu@gmail.com>
2024-06-10 14:45:59 -0400

    Colors updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './styles/Home.css'
 import { useTypewriter, Cursor} from 'react-simple-typewriter'
 import vidBackground from './assets/background.mp4'
-import background from './assets/background.png'
+import background from './assets/image.png'
 import Navbar from './components/Navbar.jsx'
 import { Link } from "react-router-dom";
 
